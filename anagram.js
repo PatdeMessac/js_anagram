f97062f52de80190f64925c269ab324a6ebda07c
Patrice <patriceguernon@free.fr>
2016-07-13 00:15:35 +0200

    initial avec anagram recherche google

--- anagram.js
+++ anagram.js
@@ -4,14 +4,50 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-'use strict';
+//'use strict';
 
-function Anagram(word) {
-  this.word = word;
+//function Anagram(word) {
+ // this.word = word;
+//}
+
+//Anagram.prototype.matches = function (words) {
+
+function anagram(s1, s2){
+  if (s1.length !== s2.length) {
+    // not the same length, can't be anagram  (poser les bases ! )
+    return false;
+  }
+  if (s1 === s2) {
+    // same string must be anagram   (évidence : niche est l'anagramme de ... niche !)
+    return true;
+  }
+
+  var c = '',
+    i = 0,
+    limit = s1.length,
+    match = 0,
+    idx;
+    
+  while(i < s1.length){
+    // chomp the next character
+    c = s1.substr(i++, 1);
+    // find it in the second string
+    idx = s2.indexOf(c);
+    if (idx > -1) {
+      // found it, add to the match
+      match++;
+      // assign the second string to remove the character we just matched
+      s2 = s2.substr(0, idx) + s2.substr(idx + 1);
+    } else {
+      // not found, not the same
+      return false;
+    }
+  }
+  return match === s1.length;
 }
 
-Anagram.prototype.matches = function (words) {
+
 //
 // YOUR CODE GOES HERE
 //
-}
+//}
